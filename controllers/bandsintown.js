5a8d322e7fa92e30895b0e3c22957f09f81ee03c
Lovino Maxime <maximelovino@gmail.com>
2017-11-14 22:27:47 +0100

    Added bandsintown controller

---
+++ controllers/bandsintown.js
@@ -0,0 +1,44 @@
+const APP_ID = "FestivDotAl";
+const request = require('request');
+
+exports.getSingleArtist = (artistName, callback) => {
+	const options = {
+		method: 'GET',
+		url: `https://rest.bandsintown.com/artists/${artistName}?app_id=${APP_ID}`,
+		headers: {
+			'Accept': "application/json",
+		}
+	}
+	
+	request(options, (error, response, body) => {
+		if (!error && response.statusCode == 200){
+			const data = JSON.parse(body);
+			callback(data);
+		}else{
+			console.log("Problem getting artist from bandsInTown");
+			console.log(error);
+			console.log(response);
+		}
+	});
+}
+
+exports.getEventsForArtist = (artistName, callback) => {
+	const options = {
+		method: 'GET',
+		url: `https://rest.bandsintown.com/artists/${artistName}/events?app_id=${APP_ID}`,
+		headers: {
+			'Accept': "application/json",
+		}
+	}
+	
+	request(options, (error, response, body) => {
+		if (!error && response.statusCode == 200){
+			const data = JSON.parse(body);
+			callback(data);
+		}else{
+			console.log("Problem getting events for artist from bandsInTown");
+			console.log(error);
+			console.log(response);
+		}
+	});
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config({path: "keys.env"});
 const express = require('express');
 const app = express();
-const spotify = require('./controllers/spotify')
+const spotify = require('./controllers/spotify');
+const bandsInTown = require('./controllers/bandsintown');
 app.set('view engine', 'pug');
 
 app.use('/material', express.static(__dirname + '/node_modules/material-components-web/dist/'));
@@ -12,6 +13,22 @@ app.use('/css', express.static(__dirname + '/css/'));
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+//temporary route to test what's given by API
+app.get('/bit/:name', (req,res) => {
+    bandsInTown.getSingleArtist(req.params.name, (data) => {
+        res.contentType('application/json');
+        res.send(data);
+    });
+});
+
+//temporary route to test what's given by API
+app.get('/bit/events/:name', (req,res) => {
+    bandsInTown.getEventsForArtist(req.params.name, (data) => {
+        res.contentType('application/json');
+        res.send(data);
+    })
+})
 
 //Route to display the picture of an artist
 app.get('/artist/:name/picture/', (req, res) => {

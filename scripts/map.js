2bbe92fc59647ce34e91e2ccd53b9a36df2e2334
Lovino Maxime <maximelovino@gmail.com>
2017-11-16 18:47:03 +0100

    Added some interactions on the map, basic music playing client side

--- scripts/map.js
+++ scripts/map.js
@@ -6,19 +6,65 @@ if (navigator.geolocation) {
     });
 }
 
+function eventOver() {
+    console.log(this);
+    const request = new Request(`/artist/${this.event_id}/song`);
+    const previewAudio = document.querySelector('#preview');
+    console.log(request);
+    fetch(request).then((response) => response.json()).then(data => {
+        console.log(data);
+        previewAudio.src = data.preview_link;
+        previewAudio.load();
+        previewAudio.play();
+    });
+}
+
+function mapMoved() {
+    const bounds = this.getBounds();
+
+    const center = bounds.getCenter();
+    const ne = bounds.getNorthEast();
+
+    // r = radius of the earth in statute miles
+    const r = 3963.0;
+
+    // Convert lat and lng from decimal degrees into radians
+    const lat1 = center.lat() * Math.PI / 180;
+    const lon1 = center.lng() * Math.PI / 180;
+    const lat2 = ne.lat() * Math.PI / 180;
+    const lon2 = ne.lng() * Math.PI / 180;
+
+    // distance = circle radius from center to Northeast corner of bounds
+    const dis = r * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1));
+    //dis contains the radius shown on the map in miles
+    console.log(`${dis} miles`);
+}
+
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: { lat: 0, lng: 0 }
     });
+
+    map.addListener('bounds_changed', mapMoved);
+
+
     let epflMarker = new google.maps.Marker({
         position: { lat: 46.5189902, lng: 6.5654067 },
         map: map,
     });
+    //the event id is an artist for now, just for testing purposes
+    epflMarker.event_id = "Muse";
+
+    epflMarker.addListener('mouseover', eventOver);
 
     let hepiaMarker = new google.maps.Marker({
         position: { lat: 46.2094937, lng: 6.133018 },
         map: map,
     });
+
+    hepiaMarker.event_id = "Linkin Park";
+    hepiaMarker.addListener('mouseover', eventOver);
 
 }

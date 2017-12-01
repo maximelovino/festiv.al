0bc3f755e105ab052220ff6e02aa912ccc09d29d
Lovino Maxime <maximelovino@gmail.com>
2017-12-01 16:58:34 +0100

    Small progress on Front End

--- client/app.js
+++ client/app.js
@@ -3,13 +3,14 @@ const app = express();
 const routes = require('./routes/index');
 
 app.set('view engine', 'pug');
-app.set('view options', {"pretty":true});
+app.set('view options', { "pretty": true });
 app.locals.pretty = true;
 
 app.use('/material', express.static(__dirname + '/node_modules/material-components-web/dist/'));
 app.use('/scripts', express.static(__dirname + '/scripts/'));
 app.use('/css', express.static(__dirname + '/css/'));
+app.use('/assets', express.static(__dirname + '/assets/'));
 
-app.use('/',routes);
+app.use('/', routes);
 
 module.exports = app;
--- client/scripts/mapDetailView.js
+++ client/scripts/mapDetailView.js
@@ -2,21 +2,30 @@ let map;
 const artistEntries = Array.from(document.querySelectorAll('.artistEntry'));
 const artistDetail = document.querySelector('#artistDetail');
 const baseURLDetail = "http://localhost:3000";
+const elevationClasses = ['mdc-elevation-transition', 'mdc-elevation--z20'];
 
 function generateArtistDetailEntry(artist) {
 	console.log(artist);
 	while (artistDetail.firstChild) {
 		artistDetail.removeChild(artistDetail.firstChild);
 	}
-	const title = document.createElement('h1');
+	const title = document.createElement('p');
 	title.innerHTML = artist.name;
+	title.classList.add('mdc-typography--headline');
 	artistDetail.appendChild(title);
+	const otherInfos = document.createElement('p');
+	otherInfos.classList.add('mdc-typography--headline');
+	otherInfos.innerHTML = `${artist.country || "Unknown country"} - ${artist.year || "Unknown year"}`;
+	artistDetail.appendChild(otherInfos);
 	if (artist.genres.length != 0) {
-		const genres = document.createElement('h3');
+		const genres = document.createElement('p');
+		genres.classList.add('mdc-typography--headline');
 		genres.innerHTML = "Genres";
 		const genreList = document.createElement('ul');
+		genreList.classList.add('mdc-list');
 		artist.genres.forEach(g => {
 			const gEntry = document.createElement('li');
+			gEntry.classList.add('mdc-list-item');
 			gEntry.innerHTML = g;
 			genreList.appendChild(gEntry);
 		});
@@ -27,7 +36,10 @@ function generateArtistDetailEntry(artist) {
 	if (artist.facebook != "") {
 		const fbLink = document.createElement('a');
 		fbLink.setAttribute('href', artist.facebook);
-		fbLink.innerHTML = "<h3>Facebook page</h3>";
+		const fbImage = document.createElement('img');
+		fbImage.setAttribute('src', "/assets/fb.png");
+		fbLink.appendChild(fbImage);
+		fbLink.classList.add("social-link");
 		artistDetail.appendChild(fbLink);
 	}
 }
@@ -37,6 +49,8 @@ artistEntries.forEach(entry => {
 	entry.addEventListener('click', function () {
 		console.log(this.dataset.artist);
 		const req = new Request(`${baseURLDetail}/artist/${this.dataset.artist}/infos`);
+		artistEntries.forEach(a => elevationClasses.forEach(cl => a.classList.remove(cl)));
+		elevationClasses.forEach(cl => this.classList.add(cl));
 		fetch(req).then(body => body.json()).then(data => {
 			generateArtistDetailEntry(data);
 		});
